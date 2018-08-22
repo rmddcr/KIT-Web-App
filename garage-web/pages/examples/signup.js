2a8c8bb0e7baa42d3e919f8d0a47b19f14fce15d
Damitha Ranasinghe <rmddcr@gmail.com>
2018-08-23 04:18:21 +0530

    done signup

--- garage-web/pages/examples/signup.js
+++ garage-web/pages/examples/signup.js
@@ -1,8 +1,26 @@
 function signup() {
    
     var userEmail =document.getElementById("myusername").value;
+    var garageName =document.getElementById("garageName").value;
     var userPassword =document.getElementById("mypassword").value;
+    var userPassword2 =document.getElementById("mypassword2").value;
+    var phoneNO =document.getElementById("contact_no").value;
     window.alert(userEmail);
+    var location_ll =document.getElementById('info').innerHTML;
+    window.alert(userEmail+" "+garageName+" "+phoneNO+" "+location_ll);
+
+
+    var database = firebase.database();
+    firebase.database().ref('Garage/' + garageName).set({
+        name: garageName,
+        email: userEmail,
+        contact_number: phoneNO,
+        address: userEmail,
+        location:location_ll
+      });
+
+
+
 
     firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
         // Handle Errors here.
@@ -15,3 +33,5 @@ function signup() {
       
     
 }
+
+
